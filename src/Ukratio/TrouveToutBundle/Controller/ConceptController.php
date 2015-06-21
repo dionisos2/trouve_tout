dionisos2/trouve_tout

commit 8a4423cdf0896f658a99513ead1596060acee617

refactorize services and forms

src/Ukratio/TrouveToutBundle/Form/EventListener/ConceptEventSubscriber.php
@@ -14,21 +14,60 @@ use Ukratio\TrouveToutBundle\Entity\Concept;
 use Ukratio\TrouveToutBundle\Entity\ConceptRepository;
 use Ukratio\TrouveToutBundle\Form\Type\ConceptConceptType;
 
-class AddCategories implements EventSubscriberInterface
+class ConceptEventSubscriber implements EventSubscriberInterface
 {
-    private $factory;
-    private $conceptRepo;
+    protected $factory;
+    protected $conceptRepo;
+    protected $entityManager;
+    protected $conceptConceptType;
 
-    public function __construct(FormFactoryInterface $factory,ConceptRepository $conceptRepo, EntityManager $entityManager)
+    public function __construct(FormFactoryInterface $factory,ConceptRepository $conceptRepo, EntityManager $entityManager, ConceptConceptType $conceptConceptType)
     {
         $this->factory = $factory;
         $this->conceptRepo = $conceptRepo;
         $this->entityManager = $entityManager;
+        $this->conceptConceptType = $conceptConceptType;
     }
 
     public static function getSubscribedEvents()
     {
         return array(FormEvents::PRE_SET_DATA => 'doAction');
+        return array(FormEvents::POST_BIND => 'postBind');
+    }
+
+    public function postBind(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        if ($data === null) {
+            return;
+        }
+
+        if (! $data instanceof Concept) {//theorically impossible
+            return;
+        }
+
+        $this->addCaractsForAllCategories($data);
+    }
+
+    public function addCaractsForAllCategories(Concept $concept)
+    {
+        foreach($concept->getMoreGeneralConcepts() as $generalCategory) {
+            $this->addCaractsOfCategory($concept, $generalCategory);
+        }
+    }
+
+    private function addCaractsOfCategory(Concept $concept, Concept $category)
+    {
+        foreach($category->getCaracts() as $caract) {
+            $name = $caract->getName();
+            if ($caract->getSelected() and ($concept->getCaract($name) == null)) {
+                $caract->setSelected($caract->getByDefault());
+                $caract = clone $caract;
+                $concept->addCaract($caract);
+            }
+        }
     }
 
     public function doAction(FormEvent $event)
@@ -40,7 +79,7 @@ class AddCategories implements EventSubscriberInterface
                          'childConcept' => $data
         );
 
-        $named = $this->factory->createNamed('moreGeneralConceptConcepts', 'collection', null, array('type' => new ConceptConceptType($this->conceptRepo, $this->entityManager),
+        $named = $this->factory->createNamed('moreGeneralConceptConcepts', 'collection', null, array('type' => $this->conceptConceptType,
                                                                                                      'label' => ' ',
                                                                                                      'allow_add' => true,
                                                                                                      'allow_delete' => true,

src/Ukratio/TrouveToutBundle/Controller/TutorialController.php
@@ -22,7 +22,7 @@ use Ukratio\TrouveToutBundle\Entity\Element;
 use Ukratio\TrouveToutBundle\Entity\Caract;
 use Ukratio\TrouveToutBundle\Entity\Type;
 use Ukratio\ToolBundle\Form\Type\ChoiceOrTextType;
-use Ukratio\TrouveToutBundle\Form\EventListener\AddCaractsOfCategories;
+use Ukratio\TrouveToutBundle\Form\EventListener\ConceptEventSubscriber;
 use Ukratio\TrouveToutBundle\Entity\Discriminator;
 
 class TutorialController extends ControllerWithTools
@@ -149,7 +149,7 @@ class TutorialController extends ControllerWithTools
 
             $concept = $this->getWardrobeCategoryBegin();
 
-            $subscriber = new AddCaractsOfCategories;
+            $subscriber = new ConceptEventSubscriber;
             $subscriber->addCaractsForAllCategories($concept);
 
             $this->get('TrouveTout.tutorial.repository.concept')->setConceptsByProperties(
@@ -393,7 +393,7 @@ class TutorialController extends ControllerWithTools
     private function getWardrobeCategoryModified()
     {
         $concept = $this->getWardrobeCategoryBegin();
-        $subscriber = new AddCaractsOfCategories;
+        $subscriber = new ConceptEventSubscriber;
         $subscriber->addCaractsForAllCategories($concept);
 
         $matter = new Element($this->trans('tutorial.input.matter'));

src/Ukratio/TrouveToutBundle/Form/EventListener/AddCaractsOfCategories.php
@@ -1,60 +1,0 @@
-<?php
-
-namespace Ukratio\TrouveToutBundle\Form\EventListener;
-
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormEvent;
-use Doctrine\ORM\EntityManager;
-
-use Ukratio\TrouveToutBundle\Entity\Element;
-use Ukratio\TrouveToutBundle\Entity\Concept;
-
-class AddCaractsOfCategories implements EventSubscriberInterface
-{
-    public function __construct()
-    {
-    }
-
-    public static function getSubscribedEvents()
-    {
-        return array(FormEvents::POST_BIND => 'postBind');
-    }
-
-    public function postBind(FormEvent $event)
-    {
-        $data = $event->getData();
-        $form = $event->getForm();
-
-        if ($data === null) {
-            return;
-        }
-
-        if (! $data instanceof Concept) {//theorically impossible
-            return;
-        }
-
-        $this->addCaractsForAllCategories($data);
-    }
-
-    public function addCaractsForAllCategories(Concept $concept)
-    {
-        foreach($concept->getMoreGeneralConcepts() as $generalCategory) {
-            $this->addCaractsOfCategory($concept, $generalCategory);
-        }
-    }
-
-    private function addCaractsOfCategory(Concept $concept, Concept $category)
-    {
-        foreach($category->getCaracts() as $caract) {
-            $name = $caract->getName();
-            if ($caract->getSelected() and ($concept->getCaract($name) == null)) {
-                $caract->setSelected($caract->getByDefault());
-                $caract = clone $caract;
-                $concept->addCaract($caract);
-            }
-        }
-    }
-}
-

src/Ukratio/TrouveToutBundle/Form/EventListener/AddChildElementSubscriber.php
@@ -1,63 +1,0 @@
-<?php
-
-namespace Ukratio\TrouveToutBundle\Form\EventListener;
-
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Form\FormEvents;
-use Doctrine\ORM\EntityManager;
-
-use Ukratio\TrouveToutBundle\Entity\Element;
-use Ukratio\TrouveToutBundle\Entity\ElementRepository;
-use Ukratio\TrouveToutBundle\Entity\Type;
-use Ukratio\TrouveToutBundle\Service\CaractTypeManager;
-use Ukratio\ToolBundle\Form\DataTransformer\StringToChoiceOrTextTransformer;
-
-class AddChildElementSubscriber implements EventSubscriberInterface
-{
-    private $factory;
-    private $type;
-    private $caractTypeManager;
-
-    public function __construct(FormFactoryInterface $factory, Type $type, CaractTypeManager $caractTypeManager)
-    {
-        $this->type = $type;
-        $this->factory = $factory;
-        $this->caractTypeManager = $caractTypeManager;
-    }
-
-    public static function getSubscribedEvents()
-    {
-        return array(FormEvents::PRE_SET_DATA => 'preSetData',
-                     FormEvents::PRE_BIND => 'preBind');
-    }
-
-    public function preBind(FormEvent $event)
-    {
-        $data = $event->getData();
-        $form = $event->getForm();
-
-        if (isset($data['childValue'])) {
-            unset($data['childValue']);
-        }
-
-        $event->setData($data); // don’t forget this
-    }
-
-    public function preSetData(FormEvent $event)
-    {
-        $data = $event->getData();
-        $form = $event->getForm();
-
-        if (! $data instanceof Element) {
-            return;
-        }
-
-        $childElement = $this->caractTypeManager->createElementForm('childValue', $this->type, $data->getPath(), 'element.specify', false);
-
-        if($childElement != null) {
-            $form->add($childElement);
-        }
-    }
-}

src/Ukratio/TrouveToutBundle/Form/EventListener/AddOwnerElementSubscriber.php
@@ -1,78 +1,0 @@
-<?php
-
-namespace Ukratio\TrouveToutBundle\Form\EventListener;
-
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Form\FormEvents;
-use Doctrine\ORM\EntityManager;
-
-use Ukratio\TrouveToutBundle\Entity\Element;
-
-class AddOwnerElementSubscriber implements EventSubscriberInterface
-{
-    private $factory;
-
-    public function __construct(FormFactoryInterface $factory)
-    {
-        $this->factory = $factory;
-    }
-
-    public static function getSubscribedEvents()
-    {
-        return array(FormEvents::PRE_SET_DATA => 'preSetData',
-                     FormEvents::PRE_BIND => 'preBind');
-    }
-
-    public function preBind(FormEvent $event)
-    {
-        $data = $event->getData();
-        $form = $event->getForm();
-
-        $ownerElements = array();
-        $index = 0;
-
-        while (isset($data['element_'.$index])) {
-            $ownerElements['element_'.$index] = $data['element_'.$index];
-            $index++;
-        }
-
-        krsort($ownerElements);
-
-        foreach ($ownerElements as $key => $ownerElement) {
-            $form->add($this->factory->createNamed($key, 'text', $ownerElement, array('mapped' => false,
-                                                                                      'label' => ' ',
-                                                                                      'read_only' => true,
-                                                                                      'auto_initialize' => false)));
-        }
-    }
-
-    public function preSetData(FormEvent $event)
-    {
-        $data = $event->getData();
-        $form = $event->getForm();
-
-        if (! $data instanceof Element) {
-            return;
-        }
-
-
-        $ownerElements = array_slice($data->getPath(), 1);
-
-        $ownerElements = array_reverse($ownerElements);
-        krsort($ownerElements);
-
-        foreach ($ownerElements as $key => $pathElement) {
-            $optionsElement = array('label' => ' ',
-                                    'read_only' => true,
-                                    'mapped' => false,
-                                    'auto_initialize' => false,
-            );
-
-            $builder = $this->factory->createNamedBuilder("element_$key", 'text', $pathElement, $optionsElement);
-            $form->add($builder->getForm());
-        }
-
-    }
-}

src/Ukratio/TrouveToutBundle/Form/EventListener/CaractEventSubscriber.php
@@ -27,7 +27,7 @@ use Ukratio\TrouveToutBundle\Service\Tools;
 
 use Doctrine\ORM\EntityManager;
 
-class AddValueSubscriber implements EventSubscriberInterface
+class CaractEventSubscriber implements EventSubscriberInterface
 {
     protected $factory;
     protected $elementRepo;
@@ -46,7 +46,7 @@ class AddValueSubscriber implements EventSubscriberInterface
     {
         return array(FormEvents::PRE_SET_DATA => 'preSet',
                      FormEvents::PRE_BIND => 'preBind',
-                     /* FormEvents::POST_BIND => 'postSubmit' */);
+                     FormEvents::POST_BIND => 'postBind');
     }
 
     public function postSubmit(FormEvent $event)
@@ -151,6 +151,63 @@ class AddValueSubscriber implements EventSubscriberInterface
         }
     }
 
+    public function postBind(FormEvent $event)
+    {
+        $caract = $event->getData();
+        $form = $event->getForm();
+
+        if (! $caract instanceof Caract) {
+            return;
+        }
+
+        if ((!$form->has('value')) || ($form->get('value') === null)) {
+            return;
+        }
+
+        //let CaractEventSubscriber do the work if we have a picture
+        if (Type::getEnumerator($caract->getType()) === Type::$picture) {
+            $picture = $form->get('choosePicture')->getData();
+            if ($picture != null) {
+                return;
+            }
+        }
+
+        $element = $form->get('value')->getData();
+
+        $ownerElements = array();
+        $index = 0;
+        while ($form->get('value')->has('element_'.$index)) {
+            $ownerElements['element_'.$index] = $form->get('value')->get('element_'.$index)->getData();
+            $index++;
+        }
+
+        $ownerElements = array_filter($ownerElements, function ($elementValue) {return $elementValue !== null;});
+
+        $pathElement = array_values($ownerElements);
+
+        if ($element->getValue() != null) {
+            $pathElement[] = $element->getValue();
+        } else {
+            $caract->setValue(null);
+            return;
+        }
+
+        $pathElement = array_reverse($pathElement);
+
+        $trueElement = $this->elementRepo->findByPath($pathElement, true);
+
+        if ($trueElement !== null) {
+            $caract->setValue($trueElement);
+        } else {//creation
+            if ($caract->getType() == Type::$date->getName()) {
+                $pathElement = array($pathElement[0], 'date');
+            }
+
+            $trueElement = $this->createElementByPath($pathElement);
+            $caract->setValue($trueElement);
+        }
+    }
+
     public function createElementByPath($pathElement)
     {
         $value = $pathElement[0];
@@ -168,4 +225,5 @@ class AddValueSubscriber implements EventSubscriberInterface
 
         return $newElement;
     }
+
 }

src/Ukratio/TrouveToutBundle/Form/EventListener/ElementEventSubscriber.php
@@ -16,21 +16,21 @@ use Ukratio\TrouveToutBundle\Entity\ConceptRepository;
 use Ukratio\TrouveToutBundle\Entity\Type;
 use Ukratio\TrouveToutBundle\Service\CaractTypeManager;
 
-class AddElementSubscriber implements EventSubscriberInterface
+class ElementEventSubscriber implements EventSubscriberInterface
 {
     private $factory;
-    private $elementRepo;
     private $type;
-    private $conceptRepo;
     private $caractTypeManager;
 
-    public function __construct(FormFactoryInterface $factory, ConceptRepository $conceptRepo, ElementRepository $elementRepo , Type $type, CaractTypeManager $caractTypeManager)
+    public function __construct(FormFactoryInterface $factory, CaractTypeManager $caractTypeManager)
     {
         $this->factory = $factory;
-        $this->elementRepo = $elementRepo;
-        $this->conceptRepo = $conceptRepo;
-        $this->type = $type;
         $this->caractTypeManager = $caractTypeManager;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
     }
 
     public static function getSubscribedEvents()
@@ -43,6 +43,30 @@ class AddElementSubscriber implements EventSubscriberInterface
     {
         $data = $event->getData();
         $form = $event->getForm();
+
+        $ownerElements = array();
+        $index = 0;
+
+        while (isset($data['element_'.$index])) {
+            $ownerElements['element_'.$index] = $data['element_'.$index];
+            $index++;
+        }
+
+        krsort($ownerElements);
+
+        foreach ($ownerElements as $key => $ownerElement) {
+            $form->add($this->factory->createNamed($key, 'text', $ownerElement, array('mapped' => false,
+                                                                                      'label' => ' ',
+                                                                                      'read_only' => true,
+                                                                                      'auto_initialize' => false)));
+        }
+
+
+        if (isset($data['childValue'])) {
+            unset($data['childValue']);
+        }
+
+        $event->setData($data); // don’t forget this
 
         if (! isset($data['value'])) {
             $form->add($this->caractTypeManager->createElementForm('value', $this->type, null));
@@ -81,6 +105,29 @@ class AddElementSubscriber implements EventSubscriberInterface
             $path = array();
         } else {
             $path = array_slice($data->getPath(), 1);
+
+            $ownerElements = array_slice($data->getPath(), 1);
+
+            $ownerElements = array_reverse($ownerElements);
+            krsort($ownerElements);
+
+            foreach ($ownerElements as $key => $pathElement) {
+                $optionsElement = array('label' => ' ',
+                                        'read_only' => true,
+                                        'mapped' => false,
+                                        'auto_initialize' => false,
+                );
+
+                $builder = $this->factory->createNamedBuilder("element_$key", 'text', $pathElement, $optionsElement);
+                $form->add($builder->getForm());
+            }
+
+
+            $childElement = $this->caractTypeManager->createElementForm('childValue', $this->type, $data->getPath(), 'element.specify', false);
+
+            if($childElement != null) {
+                $form->add($childElement);
+            }
         }
 
         $form->add($this->caractTypeManager->createElementForm('value', $this->type, $path));

src/Ukratio/TrouveToutBundle/Form/EventListener/SortedConceptEventSubscriber.php
@@ -11,9 +11,11 @@ use Doctrine\ORM\EntityManager;
 use Ukratio\TrouveToutBundle\Entity\Concept;
 use Ukratio\TrouveToutBundle\Entity\ConceptConcept;
 
-class CategoriesGetter implements EventSubscriberInterface
+class SortedConceptEventSubscriber implements EventSubscriberInterface
 {
-    private $categories;
+    protected $categories;
+    protected $conceptRepo;
+    protected $entityManager;
 
     public function __construct($categories, $conceptRepo, $entityManager)
     {

commit 7bc7298574f30c7accda44a9dd2bbbc7b2ac4f98

fix bug introduced in commit "8a4423c" : category’s heritage doesn’t work

src/Ukratio/TrouveToutBundle/Controller/ConceptController.php
@@ -97,34 +97,32 @@ class ConceptController extends ControllerWithTools
     {
         $response = new Response();
 
-        /* if ($this->get('security.context')->isGranted('ROLE_USER')) { */
-        /*     $response->setETag("connected"); */
-        /* } else { */
-        /*     $response->setETag("unconnected"); */
-        /* } */
+        if ($this->get('security.context')->isGranted('ROLE_USER')) {
+            $response->setETag("connected");
+        } else {
+            $response->setETag("unconnected");
+        }
 
         $conceptRepo = $this->getDoctrine()
                             ->getRepository('TrouveToutBundle:Concept');
-        /* if (in_array($response->getEtag(), $request->getEtags())) { */
-        /*     $date = $conceptRepo->getModifiedAt($conceptId); */
-        /*     if($date == null) { */
-        /*         $date = new \DateTime(); */
-        /*         $date->setTimezone(new \DateTimeZone('UTC')); */
-        /*     } */
-        /*     $response->setLastModified($date); */
-        /* } */
+        if (in_array($response->getEtag(), $request->getEtags())) {
+            $date = $conceptRepo->getModifiedAt($conceptId);
+            if($date == null) {
+                $date = new \DateTime();
+                $date->setTimezone(new \DateTimeZone('UTC'));
+            }
+            $response->setLastModified($date);
+        }
 
-        /* if ($response->isNotModified($request)) { */
-        /*     return $response; */
-        /* } else { */
+        if ($response->isNotModified($request)) {
+            return $response;
+        } else {
             $concept = $conceptRepo->findByIdWithCaract($conceptId);
             $cfm = $this->get('TrouveTout.ConceptFormManager');
-            echo "before createform</br>";
             $form = $cfm->createForm($concept);
-            echo "after createform</br>";
             $options = $cfm->arrayForTemplate($concept, $form);
             return $this->render('TrouveToutBundle:TrouveTout:modifyConcept.html.twig', $options, $response);
-        /* } */
+        }
 
     }
 
@@ -142,9 +140,7 @@ class ConceptController extends ControllerWithTools
 
         $form = $cfm->createForm($concept);
 
-        echo "<br>bind<br>";
         $form->bind($request);
-        echo "<br>fin bind<br>";
 
         $type = Discriminator::getEnumerator($concept->getType());
 

src/Ukratio/TrouveToutBundle/Form/EventListener/ConceptEventSubscriber.php
@@ -20,6 +20,7 @@ class ConceptEventSubscriber implements EventSubscriberInterface
     protected $conceptRepo;
     protected $entityManager;
     protected $conceptConceptType;
+    protected $hihi;
 
     public function __construct(FormFactoryInterface $factory,ConceptRepository $conceptRepo, EntityManager $entityManager, ConceptConceptType $conceptConceptType)
     {
@@ -27,12 +28,13 @@ class ConceptEventSubscriber implements EventSubscriberInterface
         $this->conceptRepo = $conceptRepo;
         $this->entityManager = $entityManager;
         $this->conceptConceptType = $conceptConceptType;
+        $this->hihi = "theEvent";
     }
 
     public static function getSubscribedEvents()
     {
-        return array(FormEvents::PRE_SET_DATA => 'preSetData');
-        return array(FormEvents::POST_SUBMIT => 'postSubmit');
+        return array(FormEvents::PRE_SET_DATA => 'preSetData',
+                     FormEvents::POST_SUBMIT => 'postSubmit');
     }
 
     public function postSubmit(FormEvent $event)
@@ -47,8 +49,7 @@ class ConceptEventSubscriber implements EventSubscriberInterface
         if (! $data instanceof Concept) {//theorically impossible
             return;
         }
-        echo "uanierstuarnietrsunateiuie";
-        throw new UnexpectedTypeException($data, 'Caract');
+
         $this->addCaractsForAllCategories($data);
     }
 

todo.org
@@ -127,8 +127,12 @@
 	première catégorie 
 ** DONE régler le problème de suppression de concepts
    CLOSED: [2015-06-03 mer. 12:20]
-** TODO régler le problème de non affichage des erreurs dans les events
-** TODO régler le problème de non-héritage de catégories
+** DONE régler le problème de non affichage des erreurs dans les events
+   CLOSED: [2015-06-21 dim. 17:06]
+   l’évènement n’était pas appelé
+** DONE régler le problème de non-héritage de catégories
+   CLOSED: [2015-06-21 dim. 17:07]
+   l’évènement n’était pas appelé
 ** TODO faire en sorte que les recherches prennent en compte l’imprécision
 ** TODO mettre symfony2 à jour
 ** TODO créer testes unitaires couvrant le programme